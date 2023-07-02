cd9fcdadcbdb8a0096882b3a0362055b642b91dd
vicok2003 <vicok2003@gmail.com>
2023-07-02 13:16:46 +0100

    working on the contact form

--- index.js
+++ index.js
@@ -118,3 +118,14 @@ function showSlides() {
 // showSlide(currentSlide);
 
 
+
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
